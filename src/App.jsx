f7dec933b56b26167b56acfbd314a4714766aa3e
Sumithra567 <sumithrakumariui@gmail.com>
2025-09-01 13:51:47 +0530

    faqapp

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Faqs from "./componentsfaqs/faqs/index.jsx"
+import "./App.css"
+function App(){
+  return(
+    <div className="backgrounds">
+      <Faqs/>
+    </div>
+    
+  )
+}
+export default App
